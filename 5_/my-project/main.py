63547942960a4b3a5993f94c796699dc7b7bdbc9
shubhra-opensource <shubhraprakash279@gmail.com>
2026-05-09 11:31:50 +0530

    2026-05-09

--- 5_/my-project/main.py
+++ 5_/my-project/main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from my-project!")
+from pydantic import BaseModel, Field
+
+
+class WeatherQuery(BaseModel):
+    city: str = Field(..., description="City name")
+    units: str = Field(..., description="celsius or fahrenheit")
+    days_ahead: int = Field(default=0,ge=0,le=7, description="Number of days ahead")
 
 
 if __name__ == "__main__":
-    main()
+    print("#"*30)
+    weather_query = WeatherQuery(city="New York", units="celsius", days_ahead=5)
+    print(weather_query)
+    print(weather_query.city)
+
+    print("#"*30)
+    # coercion
+    weather_query = WeatherQuery(city="New York", units="celsius", days_ahead="5")
+    print(weather_query)
+    print(weather_query.days_ahead)
+    print(type(weather_query.days_ahead))
+    
+    print(WeatherQuery.model_json_schema())
+
+    ##############################################
+    # validate json, weather query.model_validate_json
+    print("#"*30)
+    json_data = """{
+        "city": "New York",
+        "units": "celsius",
+        "days_ahead": 5
+    }"""
+    print(WeatherQuery.model_validate_json(json_data))
+
+    
